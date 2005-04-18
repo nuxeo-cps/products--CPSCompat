21140a80b354fd5402b44920c6a0d3cfc5e5f8b8
sfermigier <devnull@localhost>
2005-04-18 16:59:36 +0000

    Test if we really need to apply patches (somewhat hackish).

--- __init__.py
+++ __init__.py
@@ -17,12 +17,23 @@ $Id$
 
 from zLOG import LOG, INFO
 
+import os
+from Products import CMFCore
+version_file = os.path.join(os.path.dirname(CMFCore.__file__), "version.txt")
+version = open(version_file).read().strip()
+
+# XXX: need some test here too...
+LOG('CPSCompat', INFO, "Patching for ZODB 3.4 compatibility")
 import PatchZODBTransaction
 
-import PatchCMFCoreSkinnable
-import PatchCMFCorePortalObject
-import PatchCMFCorePortalFolder
+if version == "CMF-1.5.0":
+    import PatchCMFCoreSkinnable
+    import PatchCMFCorePortalObject
+    import PatchCMFCorePortalFolder
 
-import PatchCMFDefaultDublinCore
+    import PatchCMFDefaultDublinCore
 
-LOG('CPSCompat', INFO, "Patching for Zope/CMF compatibility")
+    LOG('CPSCompat', INFO, "Patching for Zope/CMF compatibility")
+
+else:
+    LOG('CPSCompat', INFO, "Already using (hopefuly) CMF 1.5.1, not patching anything")
