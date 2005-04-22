2bae929e64a072a6a6714994c4f8ab0bd494f855
fguillaume <devnull@localhost>
2005-04-22 18:58:13 +0000

    Remove explicit tests. Each patch should test itself if it needs to be applied.

--- __init__.py
+++ __init__.py
@@ -17,24 +17,13 @@ $Id$
 
 from zLOG import LOG, INFO
 
-import os
-from Products import CMFCore
-version_file = os.path.join(os.path.dirname(CMFCore.__file__), "version.txt")
-version = open(version_file).read().strip()
-
-# XXX: need some test here too...
-LOG('CPSCompat', INFO, "Patching for ZODB 3.4 compatibility")
 import PatchZODBTransaction
 
-if version == "CMF-1.5.0":
-    import PatchCMFCoreSkinnable
-    import PatchCMFCorePortalObject
-    import PatchCMFCorePortalFolder
-    import PatchCMFCoreCMFCatalogAware
+import PatchCMFCoreSkinnable
+import PatchCMFCorePortalObject
+import PatchCMFCorePortalFolder
+import PatchCMFCoreCMFCatalogAware
 
-    import PatchCMFDefaultDublinCore
+import PatchCMFDefaultDublinCore
 
-    LOG('CPSCompat', INFO, "Patching for Zope/CMF compatibility")
-
-else:
-    LOG('CPSCompat', INFO, "Already using (hopefuly) CMF 1.5.1, not patching anything")
+LOG('CPSCompat', INFO, "Patching for Zope/CMF compatibility")
