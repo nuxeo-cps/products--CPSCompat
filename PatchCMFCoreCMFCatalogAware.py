357a1d2a309578e484938fe1d0347fbe5fd6cb38
janguenot <devnull@localhost>
2005-04-25 00:51:43 +0000

    1.5.1 excluded because of the 'skip_self' argument

--- PatchCMFCoreCMFCatalogAware.py
+++ PatchCMFCoreCMFCatalogAware.py
@@ -12,7 +12,7 @@
 ##############################################################################
 """PatchCMFCoreCMFCatalogAware
 
-reindexObjectSecutiry() optimizations: CMF >= 1.5.1
+reindexObjectSecutiry() optimizations: CMF > 1.5.1
 
 $Id$
 """
