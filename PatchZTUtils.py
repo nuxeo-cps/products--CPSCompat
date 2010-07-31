94945f1eacf64f112326171ebb8b132ba8de6e19
Georges Racinet on purity.racinet.fr <georges@racinet.fr>
2010-07-31 15:33:57 +0200

    #2187: making :utf-8:ustring automatic, like :int is

--- PatchZTUtils.py
+++ PatchZTUtils.py
@@ -1,5 +1,5 @@
 import ZTUtils
-from ZPublisher.Converters import default_encoding
+from ZPublisher.Converters import default_encoding as encoding
 from ZTUtils import make_query as original_make_query
 
 def make_query(*args, **kwargs):
@@ -7,8 +7,12 @@ def make_query(*args, **kwargs):
 
     >>> from ZPublisher import Converters
     >>> Converters.default_encoding =  'utf-8'
+
+    %3A is the URL escape fr colon
     >>> make_query(param=u'\xe9')
-    'param=%C3%A9'
+    'param%3Autf-8%3Austring=%C3%A9'
+    >>> make_query({'param:utf-8:ustring' : u'\xe9'})
+    'param%3Autf-8%3Austring=%C3%A9'
     """
 
     # See original docstring
@@ -17,10 +21,15 @@ def make_query(*args, **kwargs):
         d.update(arg)
     d.update(kwargs)
 
+    uni = dict()
+    # unicode string will migrate from d to uni (avoid side effects)
     for k, v in d.items():
         if isinstance(v, unicode):
-            d[k] = v.encode(default_encoding, 'ignore')
+            p = k.split(':', 1)[0]
+            newk = '%s:%s:ustring' % (p, encoding)
+            uni[newk] = v.encode(encoding, 'ignore')
+            del d[k]
 
-    return original_make_query(d)
+    return original_make_query(d, uni)
 
 ZTUtils.make_query = make_query
